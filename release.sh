a8f204eecbfd0c660151eba55f6fb0d4507563a0
Andreas Grapentin <andreas@grapentin.org>
2017-04-26 15:27:24 +0200

    initial commit - infrastructure to create release tarballs from iceweasel branding sources

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+pkgver=45.0
+pkgrel=1
+
+srcdir=src
+distdir=iceweasel-${pkgver}
+tarball=iceweasel_${pkgver}-${pkgrel}.branding.tar.xz
+
+rm -rf $distdir/
+mkdir -pv $distdir
+
+cp -rv $srcdir/* $distdir/
+
+rm -f ${tarball}{,.sig}
+tar -cJf $tarball $distdir
+gpg --output ${tarball}.sig --detach-sig $tarball
+rm -rf $distdir
+
+echo "dist tarball:   $tarball"
+echo "dist signature: $tarball.sig"
